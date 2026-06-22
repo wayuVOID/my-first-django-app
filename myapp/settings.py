e354d06b18c272f4cacaeba35f97416411277293
wayuVOID <wayu25489@gmail.com>
2026-06-22 10:53:11 +0700

    Configure PostgreSQL database settings using dj-database-url for Vercel

--- myapp/settings.py
+++ myapp/settings.py
@@ -73,12 +73,21 @@ WSGI_APPLICATION = 'myapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
+        conn_max_age=600,
+    )
 }
+
+# Enable SSL for PostgreSQL databases in production
+if DATABASES['default']['ENGINE'] == 'django.db.backends.postgresql':
+    DATABASES['default']['OPTIONS'] = {
+        'sslmode': 'require',
+    }
+
 
 
 # Password validation
